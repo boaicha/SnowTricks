794a5c9e5492df5e6eef7dca3b6566fd9d45478a
aicha <aichabouhouch.ab@gmail.com>
2023-10-12 22:21:38 +0200

    update html files

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\TrickRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    public function index(TrickRepository $trickRepository): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        $tricks = $trickRepository->findAll();
+        return $this->render('home/home.html.twig', [
+            'tricks'=> $tricks
         ]);
     }
 }
